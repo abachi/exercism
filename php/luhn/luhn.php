9abedc5b87f2fa5d032445bdb12641534b1552b0
Nasser Abachi <abachinasser@gmail.com>
2019-03-20 17:56:01 +0100

    Lunh Algorithm working solution

---
+++ php/luhn/luhn.php
@@ -0,0 +1,38 @@
+<?php
+
+function cleanWhitespaces($digits){
+	return str_replace(' ', '', trim($digits));
+}
+
+function isValid(String $digits){
+	
+	$cleanedDigits = cleanWhitespaces($digits);
+	
+	if(preg_match('/[^0-9]/', $cleanedDigits))
+		return false;
+
+	$digitsLength = strlen($cleanedDigits);
+	if($digitsLength < 2)
+		return false;
+	
+	$sum = 0;
+
+	for ($i=$digitsLength-1; $i > -1  ; $i--) {
+		if($digitsLength % 2 === 0){
+			if($i%2 === 0 && $i != $digitsLength-1){
+				$doubled = intval($cleanedDigits[$i])*2;
+				$sum += ($doubled>9)? $doubled-9 : $doubled;
+			}else{
+				$sum += intval($cleanedDigits[$i]);
+			}
+		}else{
+			if($i%2 !== 0 && $i != $digitsLength-1){
+				$doubled = intval($cleanedDigits[$i])*2;
+				$sum += ($doubled>9)? $doubled-9 : $doubled;
+			}else{
+				$sum += intval($cleanedDigits[$i]);
+			}
+		}
+	}
+	return $sum % 10 === 0;
+}
