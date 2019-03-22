8bc8c817c76952aeba101a6949f1e207f47340f3
Nasser Abachi <abachinasser@gmail.com>
2019-03-22 18:09:51 +0100

    use PSR-2 standards and simplify isQuestion regex

--- php/bob/Bob.php
+++ php/bob/Bob.php
@@ -1,40 +1,44 @@
 <?php
 
-class Bob{
+class Bob
+{
+    private function isQuestion($sentence)
+    {
+        return preg_match('/\?$/', trim($sentence));
+    }
 
-	private function isQuestion($sentence){
-		return preg_match('/^.+\?$/', trim($sentence));
-	}
+    private function isYelling($sentence)
+    {
+        return preg_match('/^[^a-zA-Z]*([A-Z]\s*[^a-zA-Z]*)+[^a-zA-Z]*!?$/', $sentence);
+    }
 
-	private function isYelling($sentence){
-		return preg_match('/^[^a-zA-Z]*([A-Z]\s*[^a-zA-Z]*)+[^a-zA-Z]*!?$/', $sentence);
-	}
+    private function isYellingWithQuestion($sentence)
+    {
+        return $this->isYelling($sentence) && $this->isQuestion($sentence);
+    }
 
-	private function isYellingWithQuestion($sentence){
-		return $this->isYelling($sentence) && $this->isQuestion($sentence);
-	}
+    private function isNotTalking($sentence)
+    {
+        return strlen(trim($sentence)) === 0;
+    }
+    public function respondTo($text)
+    {
+        if ($this->isNotTalking($text)) {
+            return 'Fine. Be that way!';
+        }
 
-	private function isNotTalking($sentence){
-		return strlen(trim($sentence)) === 0;
-	}
-	public function respondTo($text){
-		
-		if($this->isNotTalking($text)){
-			return 'Fine. Be that way!';
-		}
+        if ($this->isYellingWithQuestion($text)) {
+            return 'Calm down, I know what I\'m doing!';
+        }
 
-		if($this->isYellingWithQuestion($text)){
-			return 'Calm down, I know what I\'m doing!';
-		}
+        if ($this->isYelling($text)) {
+            return "Whoa, chill out!";
+        }
 
-		if($this->isYelling($text)){
-			return "Whoa, chill out!";
-		}
+        if ($this->isQuestion($text)) {
+            return 'Sure.';
+        }
 
-		if($this->isQuestion($text)){
-			return 'Sure.';
-		}
-
-		return 'Whatever.';
-	}
+        return 'Whatever.';
+    }
 }
