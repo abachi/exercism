85931fc1c6ceff9a528e1b09904cde2daba23ea9
Nasser Abachi <abachinasser@gmail.com>
2019-03-22 11:08:00 +0100

    use 'strtr' function to replace nucleotides instead of doing it manually

--- php/rna-transcription/rna-transcription.php
+++ php/rna-transcription/rna-transcription.php
@@ -1,21 +1,10 @@
 <?php
 
-function transcribe(string $nucleotide) : string {
-		switch ($nucleotide) {
-			case 'G':
-				return 'C';
-			case 'C':
-				return 'G';
-			case 'T':
-				return 'A';
-			case 'A':
-				return 'U';
-			default:
-				return $nucleotide;
-		}
-}
-
 function toRna(string $dna) : string {
-	$rna  = array_map("transcribe", str_split($dna));
-	 return implode('', $rna);
+	return strtr($dna, [
+		'G' => 'C',
+		'C' => 'G',
+		'T' => 'A',
+		'A' => 'U'
+	]);
 }
