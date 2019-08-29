ffe99287d8c4d6c88cef10c2f7e2c0337c844f27
abachidev <abachinasser@gmail.com>
2019-08-29 10:26:16 +0100

    remove unnecessary complexity

--- php/raindrops/raindrops.php
+++ php/raindrops/raindrops.php
@@ -1,26 +1,19 @@
 <?php
 
 
-function raindrops($n) : string
+function raindrops(int $n) : string
 {
     $result = '';
-    for($i=1; $i<=$n; $i++){
-        if($n % $i === 0 && in_array($i, [3, 5, 7])){
-            switch($i){
-                case 3:
-                  $result .= 'Pling';
-                break;
-                case 5:
-                  $result .= 'Plang';
-                break;
-                case 7:
-                  $result .= 'Plong';
-                break;
-            }
-        }
-    }
-    if(strlen($result) === 0)
-        return $n;
+    
+    if($n % 3 === 0)
+        $result .= 'Pling';
 
-    return $result;
+    if($n % 5 === 0)
+        $result .= 'Plang';
+
+    if($n % 7 === 0)
+        $result .= 'Plong';
+
+    
+    return $result ? $result : "$n";
 }
