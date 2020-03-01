08275682ab29068a3fb094d0568cefecedc1a557
Nasser Abachi <abachinasser@gmail.com>
2020-03-01 10:52:02 +0100

    use private key and fix code formatting

--- php/largest-series-product/largest-series-product.php
+++ php/largest-series-product/largest-series-product.php
@@ -2,41 +2,41 @@
 
 class Series
 {
-    function __construct($serie)
+    private $serie;
+
+    public function __construct($serie)
     {
-        $this->_serie = (string) $serie;
+        $this->serie = (string) $serie;
     }
 
     public function largestProduct($length) : int
-    { 
-      if(strlen($this->_serie) === 0 && $length === 0)
-        return 1;
+    {
+        if (strlen($this->serie) === 0 && $length === 0) {
+            return 1;
+        }
       
-      $this->validate($length);
+        $this->validate($length);
 
-      $n = strlen($this->_serie) - $length + 1;
-      $products = [];
-      $p = 1;
-      for($i=0; $i<$n; $i++)
-      {
-        for($j=$i; $j<($length + $i); $j++)
-        {
-          $p *= (int) $this->_serie[$j];
-         }
-         $products[] = $p; 
-         $p = 1;
-      }
-      return max($products);
-    } 
+        $n = strlen($this->serie) - $length + 1;
+        $products = [];
+        $p = 1;
+        for ($i=0; $i<$n; $i++) {
+            for ($j=$i; $j<($length + $i); $j++) {
+                $p *= (int) $this->serie[$j];
+            }
+            $products[] = $p;
+            $p = 1;
+        }
+        return max($products);
+    }
 
     private function validate($sequenceLength) : void
     {
-      $serieLength = strlen($this->_serie);
-      $notValid = !is_numeric($this->_serie) || $sequenceLength < 0  || $serieLength < $sequenceLength;
+        $serieLength = strlen($this->serie);
+        $notValid = !is_numeric($this->serie) || $sequenceLength < 0  || $serieLength < $sequenceLength;
       
-      if($notValid)
-        throw new InvalidArgumentException();
+        if ($notValid) {
+            throw new InvalidArgumentException();
+        }
     }
-
-  
 }
